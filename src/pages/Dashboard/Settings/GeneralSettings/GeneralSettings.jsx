9fc348b16ce7bf9587b122688980611515dbbbec
Saleh Shakib <55012686+salehshakib@users.noreply.github.com>
2024-04-29 12:39:28 +0600

    pro components updated

--- src/pages/Dashboard/Settings/GeneralSettings/GeneralSettings.jsx
+++ src/pages/Dashboard/Settings/GeneralSettings/GeneralSettings.jsx
@@ -1,5 +1,14 @@
+import { PageContainer } from "@ant-design/pro-layout";
+
 const GeneralSettings = () => {
-  return <div className="">GeneralSettings</div>;
+  return (
+    <PageContainer
+      title={<div className="text-2xl lg:text-3xl">Settings</div>}
+      subTitle={<div className="text-2xl lg:text-3xl">General Settings</div>}
+    >
+      <div>hello</div>
+    </PageContainer>
+  );
 };
 
 export default GeneralSettings;

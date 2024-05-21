bad12a966c6d81bbfce20bd6e5dd97f43deb3aed
Saleh Shakib <55012686+salehshakib@users.noreply.github.com>
2024-05-21 12:51:29 +0600

    logo updated

--- src/components/AllSection/Header/Logo.jsx
+++ src/components/AllSection/Header/Logo.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import logo from "../../../assets/images/logo.png";
+import logo from "../../../assets/images/Vistock-removebg-preview.png";
 
 const Logo = () => {
   const navigate = useNavigate();
@@ -8,7 +8,7 @@ const Logo = () => {
       className="font-bold hover:cursor-pointer primary-text flex justify-center items-center "
       onClick={() => navigate("/dashboard")}
     >
-      <img src={logo} alt="" className="w-32" />
+      <img src={logo} alt="" className="size-32" />
       {/* <p>POS Inventory</p> */}
     </div>
   );

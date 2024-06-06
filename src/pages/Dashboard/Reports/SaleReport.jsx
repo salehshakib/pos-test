5d73df245cd9daa91f1227749dd78681c6f5f04e
Saleh Shakib <55012686+salehshakib@users.noreply.github.com>
2024-06-06 15:53:05 +0600

    vercel check

--- src/pages/Dashboard/Reports/SaleReport.jsx
+++ src/pages/Dashboard/Reports/SaleReport.jsx
@@ -1,5 +1,7 @@
 import { ReportContainer } from "../../../container/ReportContainer/ReportContainer";
 
 export const SaleReport = () => {
-  return <ReportContainer pageTitle="Sale Report"></ReportContainer>;
+  return (
+    <ReportContainer pageTitle="Sale Report checking vercel"></ReportContainer>
+  );
 };

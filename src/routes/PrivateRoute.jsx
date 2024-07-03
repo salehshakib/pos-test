bacbfcdcfebebe3f339c694f287003bebaec709e
Saleh Shakib <55012686+salehshakib@users.noreply.github.com>
2024-07-03 18:52:37 +0600

    fix login issue

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,11 +1,13 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
-import { useCurrentToken } from "../redux/services/auth/authSlice";
+import { logout, useCurrentToken } from "../redux/services/auth/authSlice";
 
 function PrivateRoute({ children }) {
   const token = useSelector(useCurrentToken);
+  const dispatch = useDispatch();
 
   if (!token) {
+    dispatch(logout());
     return <Navigate to={"/login"} replace={true} />;
   }
 

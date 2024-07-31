8d048d02aa0b795e4ba36e7971b0806c63bdb656
Saleh Shakib <55012686+salehshakib@users.noreply.github.com>
2024-07-31 19:02:20 +0600

    working on some details

--- src/utilities/lib/getWarehouseQty.js
+++ src/utilities/lib/getWarehouseQty.js
@@ -1,8 +1,10 @@
 export function getWarehouseQuantity(data, warehouseId) {
   // console.log(data, warehouseId);
-  const item = data.find(
-    (item) => item.warehouse_id?.toString() === warehouseId?.toString()
-  );
+  const item =
+    data &&
+    data?.find(
+      (item) => item.warehouse_id?.toString() === warehouseId?.toString()
+    );
   // console.log(item);
   return item ? parseInt(item.qty) : 0;
 }

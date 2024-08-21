ced5a91fedf9a0f5ff8576ea863009828c9567d0
Saleh Shakib <55012686+salehshakib@users.noreply.github.com>
2024-08-21 16:23:34 +0600

    Update base_url.js

--- src/utilities/configs/base_url.js
+++ src/utilities/configs/base_url.js
@@ -3,11 +3,12 @@ export const isDev = import.meta.env.VITE_DEV_MODE;
 
 const apiEndpoints = {
   local: import.meta.env.VITE_LOCAL_API_URL,
-  vistock: import.meta.env.VITE_VISTOCK_API_URL,
-  pms: import.meta.env.VITE_PMS_API_URL,
-  dhakatech: import.meta.env.VITE_DHAKATECH_API_URL,
+  production: import.meta.env.VITE_PRODUCTION_API_URL,
+  // vistock: import.meta.env.VITE_VISTOCK_API_URL,
+  // pms: import.meta.env.VITE_PMS_API_URL,
+  // dhakatech: import.meta.env.VITE_DHAKATECH_API_URL,
+  // vitaport: import.meta.env.VITE_VITAPORT_API_URL,
 };
 
 export const base_url = apiEndpoints[mode];
-// export const base_url = "https://dhakatech.vitasoftsolutions.com/api";
 export const api_key = import.meta.env.VITE_SECRET_KEY;

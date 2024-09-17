b54c299fa0bae4a6883e90147bded00ae825753b
salehshakib <salehshakib.ss@gmail.com>
2024-09-17 13:43:38 +0600

    fix(translator): fixed padding

--- src/components/Shared/Translate/Translate.jsx
+++ src/components/Shared/Translate/Translate.jsx
@@ -1,14 +1,57 @@
+// import { useEffect, useRef } from 'react';
+
+// const Translate = () => {
+//   const googleTranslateRef = useRef(null);
+
+//   useEffect(() => {
+//     let intervalId;
+
+//     const checkGoogleTranslate = () => {
+//       if (window.google && window.google.translate) {
+//         clearInterval(intervalId);
+//         new window.google.translate.TranslateElement(
+//           {
+//             pageLanguage: 'en',
+//             layout:
+//               window.google.translate.TranslateElement.InlineLayout.SIMPLE,
+//           },
+//           googleTranslateRef.current
+//         );
+//       }
+//     };
+
+//     intervalId = setInterval(checkGoogleTranslate, 100);
+
+//     return () => clearInterval(intervalId);
+//   }, []);
+
+//   return (
+//     <div>
+//       <div ref={googleTranslateRef}></div>
+//     </div>
+//   );
+// };
+
+// export default Translate;
+
 import { useEffect, useRef } from 'react';
 
 const Translate = () => {
   const googleTranslateRef = useRef(null);
 
   useEffect(() => {
-    let intervalId;
+    const loadGoogleTranslateScript = () => {
+      if (!document.querySelector('script[src*="translate.google.com"]')) {
+        const script = document.createElement('script');
+        script.src =
+          'https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit';
+        script.async = true;
+        document.body.appendChild(script);
+      }
+    };
 
-    const checkGoogleTranslate = () => {
+    const initGoogleTranslate = () => {
       if (window.google && window.google.translate) {
-        clearInterval(intervalId);
         new window.google.translate.TranslateElement(
           {
             pageLanguage: 'en',
@@ -20,16 +63,16 @@ const Translate = () => {
       }
     };
 
-    intervalId = setInterval(checkGoogleTranslate, 100);
-
-    return () => clearInterval(intervalId);
+    // Check if the script is already loaded
+    if (window.google && window.google.translate) {
+      initGoogleTranslate();
+    } else {
+      window.googleTranslateElementInit = initGoogleTranslate;
+      loadGoogleTranslateScript();
+    }
   }, []);
 
-  return (
-    <div>
-      <div ref={googleTranslateRef}></div>
-    </div>
-  );
+  return <div ref={googleTranslateRef}></div>;
 };
 
 export default Translate;

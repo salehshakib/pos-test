140222be87e9d42320b6f304ac3cd7496182a660
salehshakib <salehshakib.ss@gmail.com>
2024-09-24 20:20:19 +0600

    update

--- src/components/Shared/ProductControllerComponent/SearchProduct.jsx
+++ src/components/Shared/ProductControllerComponent/SearchProduct.jsx
@@ -41,7 +41,7 @@ export const SearchProduct = ({ setProducts }) => {
     ignorePaths.filter((item) => pathname.includes(item)).length === 0;
 
   const baseParams = {
-    warehouse_id:
+    product_warehouse_id:
       pathname.includes('transfer') || pathname.includes('stock-request')
         ? warehouseIdFrom
         : warehouseId,
